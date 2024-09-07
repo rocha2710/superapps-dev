94223f5985c2bf7990232a4ef65b17dff53dc309
rocha2710 <rocha.ec@outlook.com>
2024-09-07 14:10:42 -0300

    Atualização automática

---
+++ auto-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Adiciona todas as alterações
+git add .
+
+# Commit com uma mensagem padrão
+git commit -m "Atualização automática"
+
+# Envia as alterações para o GitHub
+git push origin main
